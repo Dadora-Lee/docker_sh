a496e4dc724d975cb2c4cdec9daf7a50825c87be
Dadora-Lee <dadorasoft@gmail.com>
2025-05-12 14:21:20 +0900

    Update build_ubuntu.sh

--- build_ubuntu.sh
+++ build_ubuntu.sh
@@ -1,6 +1,2 @@
-sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
+sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev nano -y
 git clone https://github.com/xmrig/xmrig.git
-mkdir xmrig/build && cd xmrig/build
-cmake ..
-make -j$(nproc)
-sudo sysctl -w vm.nr_hugepages=1280
