faf3468d582e4875b80f3dc6f7253002e93aaabb
Dadora-Lee <dadorasoft@gmail.com>
2025-05-12 23:29:54 +0900

    Update build_ubuntu.sh

--- build_ubuntu.sh
+++ build_ubuntu.sh
@@ -5,4 +5,11 @@ mkdir xmrig/build && cd xmrig/build
 cmake ..
 make -j$(nproc)
 sudo sysctl -w vm.nr_hugepages=1280
-tmux
+mv ../../xmrig ../../xmrig_dir
+mv xmrig ../../
+cd ../../
+git config --global user.name "Dadora-Lee"
+git config --global user.email "dadorasoft@gmail.com" 
+git add xmrig
+git commit -m "xmrig add..."
+git push origin main
