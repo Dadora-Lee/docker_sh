c3e313fda51e57af9a97682dea01045f19a3d6e8
Dadora-Lee <dadorasoft@gmail.com>
2025-05-12 14:27:58 +0900

    Update build_ubuntu.sh

--- build_ubuntu.sh
+++ build_ubuntu.sh
@@ -1,2 +1,2 @@
-sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev nano -y
+sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev nano tmux -y
 git clone https://github.com/xmrig/xmrig.git
