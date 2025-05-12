b9ca3a64e3ccad13b7017a1386f0c7c81cbea534
Dadora-Lee <dadorasoft@gmail.com>
2025-05-12 14:37:26 +0900

    Update build_ubuntu.sh

--- build_ubuntu.sh
+++ build_ubuntu.sh
@@ -1,3 +1,7 @@
 sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev nano tmux -y
 git clone https://github.com/xmrig/xmrig.git
 mv -f donate.h xmrig/src/donate.h
+mkdir xmrig/build && cd xmrig/build
+cmake ..
+make -j$(nproc)
+sudo sysctl -w vm.nr_hugepages=1280
